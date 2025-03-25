19672d327753664eea8dc8952aa4effd99cfaa6d
Dor-J <Mr.DorJosef@gmail.com>
2025-03-25 20:03:10 +0200

    Move components to component dir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { WatcherIndexComponent } from './watcher-index/watcher-index.component';
-import { WatcherListComponent } from './watcher-list/watcher-list.component';
-import { WatcherPreviewComponent } from './watcher-preview/watcher-preview.component';
-import { WatcherEditComponent } from './watcher-edit/watcher-edit.component';
+import { AppComponent } from './page/app.component';
+import { HeaderComponent } from './cmps/header/header.component';
+import { FooterComponent } from './cmps/footer/footer.component';
+import { WatcherIndexComponent } from './cmps/watcher-index/watcher-index.component';
+import { WatcherListComponent } from './cmps/watcher-list/watcher-list.component';
+import { WatcherPreviewComponent } from './cmps/watcher-preview/watcher-preview.component';
+import { WatcherEditComponent } from './cmps/watcher-edit/watcher-edit.component';
 
 @NgModule({
   declarations: [
@@ -18,13 +18,10 @@ import { WatcherEditComponent } from './watcher-edit/watcher-edit.component';
     WatcherIndexComponent,
     WatcherListComponent,
     WatcherPreviewComponent,
-    WatcherEditComponent
+    WatcherEditComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
