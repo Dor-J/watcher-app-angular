257d0f9d81725d0eef213723fcb95bcb6095a462
Dor-J <Mr.DorJosef@gmail.com>
2025-03-26 13:03:30 +0200

    Add util service and refactor watcher service

---
+++ src/app/services/watcher.service.ts
@@ -0,0 +1,72 @@
+import { Watcher } from '../models/watcher.model.js';
+import { storageService } from './async-storage.service.js';
+import { loadFromStorage, saveToStorage } from './util.service.js';
+
+export const watcherService = {
+  query,
+  remove,
+  save,
+  createWatcher,
+};
+
+const WATCHER_DB = 'WATCHER_DB';
+_createWatchers();
+
+function query() {
+  return storageService.query(WATCHER_DB);
+}
+
+function remove(watcherId: string) {
+  return storageService.remove(WATCHER_DB, watcherId);
+}
+
+function save(watcher: Watcher) {
+  return storageService.post(WATCHER_DB, watcher);
+}
+
+function createWatcher(fullname: string, movies = ['A', 'B', 'C']) {
+  return {
+    fullname,
+    movies,
+  };
+}
+
+function _createWatchers() {
+  let watchers: Watcher[] = loadFromStorage(WATCHER_DB);
+  if (!watchers || !watchers.length) {
+    watchers = [
+      {
+        id: 'w101',
+        fullname: 'Puki Ba',
+        movies: ['Rambo', 'Rocky'],
+      },
+      {
+        id: 'w102',
+        fullname: 'John Doe',
+        movies: ['The Godfather', 'Breaking Bad'],
+      },
+      {
+        id: 'w103',
+        fullname: 'Jane Smith',
+        movies: ['Inception', 'Stranger Things'],
+      },
+      {
+        id: 'w104',
+        fullname: 'Alice Johnson',
+        movies: ['The Matrix', 'Game of Thrones'],
+      },
+      {
+        id: 'w105',
+        fullname: 'Bob Brown',
+        movies: ['The Dark Knight', 'The Mandalorian'],
+      },
+      {
+        id: 'w106',
+        fullname: 'Emily Davis',
+        movies: ['Pulp Fiction', 'The Crown'],
+      },
+    ];
+
+    saveToStorage(WATCHER_DB, watchers);
+  }
+}
